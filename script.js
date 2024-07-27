6700e0de4519caf167e3e2e201f55c8143adc49b
bng16 <biswajitnag161619@gmail.com>
2024-07-27 14:31:45 +0530

    1-2 done

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function fun1(){
-    const myName='bng';
-    function fun2(){
-        return myName+' '+'Nag';
-    }
-    return fun2();
+// function fun1(){
+//     const myName='bng';
+//     function fun2(){
+//         return myName+' '+'Nag';
+//     }
+//     return fun2();
+// }
+// console.log(fun1())
+
+let count=0;
+function counter(){
+    count += 1;
 }
-console.log(fun1())
+
+for (let i=0; i<10;i++){
+    counter();
+}
+
+console.log(count);
