5fce41fdb5399a2c3f35017511c7395d77166510
bng16 <biswajitnag161619@gmail.com>
2024-07-27 17:57:07 +0530

    done

--- script.js
+++ script.js
@@ -7,13 +7,28 @@
 // }
 // console.log(fun1())
 
-let count=0;
-function counter(){
-    count += 1;
+// let count=0;
+// function counter(){
+//     count += 1;
+// }
+
+// for (let i=0; i<10;i++){
+//     counter();
+// }
+
+// console.log(count);
+
+
+let lastUniqueId=546546515854;
+
+let generateId=()=>{
+    return ++lastUniqueId;
+}
+console.log(`your id is ${generateId()}`);
+console.log(lastUniqueId);
+
+for (let i=0; i<5;i++){
+    console.log(`your id is ${generateId()}`);
 }
 
-for (let i=0; i<10;i++){
-    counter();
-}
-
-console.log(count);
+console.log(lastUniqueId);
